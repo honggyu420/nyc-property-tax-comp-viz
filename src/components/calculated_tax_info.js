3d511101a7ac926f9a982fee30393200f5870d8f
honggyu420@gmail.com <honggyu420@gmail.com>
2020-06-12 21:25:19 -0400

    Update calculated_tax_info.js
    fix incorrect ETR for class 2 properties to use transitional values

--- src/components/calculated_tax_info.js
+++ src/components/calculated_tax_info.js
@@ -14,42 +14,44 @@ class CalculatedTaxInfo extends Component {
 
     return num + x.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
   }
-  
+
   calculateTaxBill(property_tax_info) {
     if (!property_tax_info) {
       return ''
     }
-    let calculated_tax_results = []
 
-    let assessed_total_val = property_tax_info.curtxbtot
-    let assessed_exemption = property_tax_info.curactextot
+    let calculated_tax_results = []
+    
+    let tax_class = property_tax_info.curtaxclass
     let market_val = property_tax_info.curmkttot
 
-    let tax_class = property_tax_info.curtaxclass
-    let class_1_tax_rate = 0.21167
-    let class_2_tax_rate = 0.12473
+    // pick trans total exemption and trans total val if tax class 2 (condo building with more than 10 units)
+    let use_trans = tax_class === '2' && property_tax_info.curtrntot && property_tax_info.curtrntot !== '0'
+    let assessed_total_val = use_trans? property_tax_info.curtrntot: property_tax_info.curtxbtot
+    let assessed_exemption = use_trans? property_tax_info.curtrnextot: property_tax_info.curactextot
 
-    let estimated_tax_bill = assessed_total_val
-    let calculated_string = this.numWithCommas(assessed_total_val)
+    if (tax_class.indexOf('1') > -1 || tax_class.indexOf('2') > -1) {
+      let class_1_tax_rate = 0.21167
+      let class_2_tax_rate = 0.12473
 
-    if (assessed_exemption !== '0') {
-      calculated_string = '(' + calculated_string + ' - ' + this.numWithCommas(assessed_exemption) + ')'
-      estimated_tax_bill -= assessed_exemption
-    }
+      let estimated_tax_bill = assessed_total_val
+      let calculated_string = this.numWithCommas(assessed_total_val)
 
-    if (tax_class.indexOf('1') > -1) {
-      estimated_tax_bill = (estimated_tax_bill * class_1_tax_rate).toFixed(2)
-      calculated_string += ' x ' + (class_1_tax_rate*100).toFixed(3) + '% = ' + this.numWithCommas(estimated_tax_bill.toString())
-    } else if (tax_class.indexOf('2') > -1) {
-      estimated_tax_bill = (estimated_tax_bill * class_2_tax_rate).toFixed(2)
-      calculated_string += ' x ' + (class_2_tax_rate*100).toFixed(3) + '% = ' + this.numWithCommas(estimated_tax_bill.toString())
-    } else {
-      calculated_string = 'N/A'
-    }
+      if (assessed_exemption !== '0') {
+        calculated_string = '(' + calculated_string + ' - ' + this.numWithCommas(assessed_exemption) + ')'
+        estimated_tax_bill -= assessed_exemption
+      }
 
-    calculated_tax_results.push(<h4>estimated 2020 tax bill: <b>{calculated_string}</b></h4>)
+      let tax_rate = class_1_tax_rate;
+      if (tax_class.indexOf('2') > -1) {
+        tax_rate = class_2_tax_rate;
+      }
 
-    if (calculated_string !== 'N/A') {
+      estimated_tax_bill = (estimated_tax_bill * tax_rate).toFixed(2)
+      calculated_string += ' x ' + (tax_rate*100).toFixed(3) + '% = ' + this.numWithCommas(estimated_tax_bill.toString())
+
+      calculated_tax_results.push(<h4>estimated 2020 tax bill: <b>{calculated_string}</b></h4>)
+
       let tax_rate_against_market_val = ((estimated_tax_bill / market_val) * 100).toFixed(2)
       calculated_tax_results.push(<h4>effective tax rate: <b>{tax_rate_against_market_val}%</b>.</h4>)
     }
