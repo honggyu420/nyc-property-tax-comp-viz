b0279723784bd75dfd5eed26c059434cd6007f52
honggyu420@gmail.com <honggyu420@gmail.com>
2020-06-24 15:10:04 -0400

    Updates

---
+++ precache-manifest.c534faf8c0fe864a8e2ddc8528d6af9c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cac47343bfec9eef15a2749b5432af13",
+    "url": "/nyc-property-tax-comp-viz/index.html"
+  },
+  {
+    "revision": "30e27c6c7b0cdb55643c",
+    "url": "/nyc-property-tax-comp-viz/static/css/main.f6eee63b.chunk.css"
+  },
+  {
+    "revision": "cc707c386f102b4b1ad5",
+    "url": "/nyc-property-tax-comp-viz/static/js/2.24735c7c.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/nyc-property-tax-comp-viz/static/js/2.24735c7c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30e27c6c7b0cdb55643c",
+    "url": "/nyc-property-tax-comp-viz/static/js/main.adbfd3bf.chunk.js"
+  },
+  {
+    "revision": "3a0eec0bc1623f55a24f",
+    "url": "/nyc-property-tax-comp-viz/static/js/runtime-main.e13dc5c0.js"
+  }
+]);
